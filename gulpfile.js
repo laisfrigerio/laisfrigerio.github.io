e0673694d5080a691e8da5ffc23392f0c1ce2cf3
Lais Frigério <laiss@gazetadopovo.com.br>
2021-08-22 13:07:43 -0300

    Add function to watch css/html changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 const gulp = require("gulp");
-const sass = require("gulp-sass");
+const sass = require("gulp-sass")(require('sass'));
 const minify = require('gulp-csso');
 const concat = require('gulp-concat');
-var pug = require('gulp-pug');
+const pug = require('gulp-pug');
 
 function css() {
-  return gulp.src("./resources/styles/app.scss")
+  return gulp.src('./resources/styles/app.scss')
     .pipe(sass({utputStyle: 'app', onError: console.error.bind(console, 'Sass error:')}))
     .pipe(minify(concat('app.css')))
-    .pipe(gulp.dest("./css"));
+    .pipe(gulp.dest('./css'));
 }
 
 function views() {
@@ -18,9 +18,15 @@ function views() {
     'resources/templates/projetos.pug',
   ])
   .pipe(pug())
-  .pipe(gulp.dest("./"));
+  .pipe(gulp.dest('./'));
+}
+
+function watch() {
+  gulp.watch('./resources/styles/**/*.scss', css)
+  gulp.watch('./resources/templates/**/*.pug', views)
 }
 
 exports.css = css;
 exports.views = views;
+exports.watch = watch
 exports.default = gulp.parallel(css, views);
